882b6051ec4b231298f7f897d7c41614a6c6b6ae
Valya <wpetrowa02@gmail.com>
2024-06-05 13:52:25 +0300

    initial commit

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -11,21 +11,38 @@ public class Menu {
     Scanner sc = new Scanner(System.in);
     DB db = new DB();
 
-    public void Start() {
+    public void start(){
+        System.out.println("Welcome to Vals' railway station!");
+        System.out.println("[REGISTER] [LOGIN]\n[TICKETS] [TRAINS] [INFO]");
         String command = sc.nextLine();
         switch (command) {
-            case "register":
+            case "register" -> {
                 try {
                     db.registerUser();
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
-            case "login":
+            }
+            case "login" -> {
                 try {
                     db.loginUser();
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
+            }
+            case "tickets" -> System.out.println("a");
+            case "trains" -> {
+                System.out.println("b");
+            }
+            default -> {
+                System.out.println("Would you like to return to the main menu? [Y][N]");
+                String answer = sc.nextLine();
+                if (answer.equals("Y")) {
+                    start();
+                } else {
+                    break;
+                }
+            }
         }
-    }
+}
 }
---
+++ src/main/java/org/example/model/Train.java
@@ -0,0 +1,52 @@
+package org.example.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Train {
+    private Place from;
+    private Place to;
+    private LocalDate date;
+    private LocalTime time;
+    private LocalTime arrival;
+    public List<Train> trains = new ArrayList<>();
+
+    public Place getFrom() {
+        return from;}
+
+    public void setFrom(Place from) {
+        this.from = from;}
+
+    public Place getTo() {
+        return to;}
+
+    public void setTo(Place to) {
+        this.to = to;}
+
+    public LocalDate getDate() {
+        return date;}
+
+    public void setDate(LocalDate date) {
+        this.date = date;}
+
+    public LocalTime getTime() {
+        return time;}
+
+    public void setTime(LocalTime time) {
+        this.time = time;}
+
+    public LocalTime getArrival() {
+        return arrival;}
+
+    public void setArrival(LocalTime arrival) {
+        this.arrival = arrival;}
+    public Train(Place from, Place to, LocalDate date, LocalTime time, LocalTime arrival) {
+        this.from = from;
+        this.to = to;
+        this.date = date;
+        this.time = time;
+        this.arrival = arrival;
+    }
+}
